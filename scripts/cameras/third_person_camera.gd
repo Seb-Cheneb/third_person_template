e947fbe0a9a34e0f4d5498fe506a743e8f2310c7
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-06-08 22:08:02 +0300

    update

---
+++ scripts/cameras/third_person_camera.gd
@@ -0,0 +1,40 @@
+class_name ThirdPersonCamera extends Node
+
+@export_range(0, 100, 1) var spring_arm_length: float = 5
+@export_range(0, 1, 0.00025) var mouse_sensitivity: float = 0.00075
+
+@export_range(-360, 360, 10, "degrees") 
+var min_boundary: float = -60:
+	set(value):
+		min_boundary = deg_to_rad(value)
+		
+@export_range(-360, 360, 10, "degrees") 
+var max_boundary: float = 10:
+	set(value):
+		max_boundary = deg_to_rad(value)
+
+var _look: Vector2 = Vector2.ZERO # stores the x / y direction the player is trying to look in
+
+@onready var horizontal_pivot: Node3D = $HorizontalPivot
+@onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
+@onready var spring_arm: SpringArm3D = $SpringArm3D
+
+
+func _ready() -> void:
+	spring_arm.spring_length = spring_arm_length
+
+
+func on_unhandled_input(event: InputEvent) -> void:
+	# only works if the mouse isn't seen (captured)
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			_look = -event.relative * mouse_sensitivity
+
+
+func on_physics_process() -> void:
+	horizontal_pivot.rotate_y(_look.x)
+	vertical_pivot.rotate_x(_look.y)
+	vertical_pivot.rotation.x = clampf(vertical_pivot.rotation.x, min_boundary, max_boundary)
+	spring_arm.global_transform = vertical_pivot.global_transform
+	_look = Vector2.ZERO
+	
--- scripts/characters/player/player.gd
+++ scripts/characters/player/player.gd
@@ -1,11 +1,23 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@export var _camera_component: ThirdPersonCamera
+@export var _animation_decay: float = 20.0
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity") # default gravity of the engine
+
+@onready var rig_pivot: Node3D = $RigPivot
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 
 func _physics_process(delta: float) -> void:
+	_camera_component.on_physics_process()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -14,15 +26,32 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	var direction := get_movement_direction()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		look_toward_direction(direction, delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	_camera_component.on_unhandled_input(event)
+
+
+func get_movement_direction() -> Vector3:
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var input_vector := Vector3(input_dir.x, 0, input_dir.y).normalized()
+	var direction := _camera_component.horizontal_pivot.global_transform.basis * input_vector
+	return direction
+
+
+func look_toward_direction(direction: Vector3, delta: float) -> void:
+	var target_transform := rig_pivot.global_transform.looking_at(rig_pivot.global_position + direction, Vector3.UP, true)
+	rig_pivot.global_transform = rig_pivot.global_transform.interpolate_with(target_transform, 1.0 - exp(-_animation_decay * delta))
+	
