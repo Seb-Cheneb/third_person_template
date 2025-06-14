1d5dc978c72786b0fa3ccf164c7619a86f1a8316
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-06-15 00:16:57 +0300

    Update submodule pointers to latest commits

--- github.sh
+++ github.sh
@@ -1,4 +1,18 @@
+#!/bin/bash
+set -e
+
+# Update and commit changes inside each submodule
+git submodule foreach '
+  git add .
+  if ! git diff --cached --quiet; then
+    git commit -m "Update submodule: $name"
+    git push origin HEAD
+  fi
+'
+
+# Back to parent repo
 git submodule update --remote --merge
 git add .
-git commit -m "Update submodules to latest commits"
+git commit -m "Update submodule pointers to latest commits"
 git push origin main
+
