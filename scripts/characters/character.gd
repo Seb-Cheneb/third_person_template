3daf6620899dc6deb89d22d15834045ad4da8d8f
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-06-15 23:59:20 +0300

    Update submodule pointers to latest commits

--- scripts/characters/character.gd
+++ scripts/characters/character.gd
@@ -1,12 +1,13 @@
 class_name Character
 extends CharacterBody3D
 
-@export
-var character_stats: CharacterStats
-
-@export
-var pivot: Node3D
+@export var character_stats: CharacterStats
+@export var pivot: Node3D
+@export var gravity_modifier: float = 1
 
 var movement_direction: Vector3 = Vector3.ZERO
 
-var gravity: Gravity = Gravity.new(self)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta * gravity_modifier
