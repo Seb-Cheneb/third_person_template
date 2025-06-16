4785278f7ff28cbf5beec6177a2e869f7e781952
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-06-16 22:54:24 +0300

    Update submodule pointers to latest commits

--- scripts/characters/player_character.gd
+++ scripts/characters/player_character.gd
@@ -1,28 +1,25 @@
 class_name PlayerCharacter
 extends Character
 
-@export
-var camera_component: ThirdPersonCamera
+@export var camera_component: ThirdPersonCamera
 
-var input_direction: Vector2 = Vector2.ZERO
-var input_vector: Vector3 = Vector3.ZERO
-
-@export
-var animation_decay: float = 10
+# Cache the previous input to detect changes and avoid unnecessary calculations
+var previous_input_direction: Vector2 = Vector2.ZERO
+var movement_direction_dirty: bool = false
 
 func _physics_process(delta: float) -> void:
-	set_movement_direction()
-	ease_toward_direction(delta)
+	update_movement_direction()
+	super._physics_process(delta)
 
-
-func set_movement_direction() -> void:
-	input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	input_vector = Vector3(input_direction.x, 0, input_direction.y).normalized()
-	movement_direction = camera_component.horizontal_pivot.global_transform.basis * input_vector
-
-
-func ease_toward_direction(delta: float) -> void:
-	# Only rotate if there's a valid movement direction
-	if not movement_direction.is_zero_approx():
-		var target_transform := pivot.global_transform.looking_at(pivot.global_position + movement_direction, Vector3.UP, true)
-		pivot.global_transform = pivot.global_transform.interpolate_with(target_transform, 1.0 - exp(-animation_decay * delta))
+func update_movement_direction() -> void:
+	var current_input = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	
+	# Only recalculate if input changed
+	if not current_input.is_equal_approx(previous_input_direction):
+		previous_input_direction = current_input
+		
+		if current_input.is_zero_approx():
+			movement_direction = Vector3.ZERO
+		else:
+			var input_vector = Vector3(current_input.x, 0, current_input.y).normalized()
+			movement_direction = camera_component.horizontal_pivot.global_transform.basis * input_vector
--- scripts/characters/states/movement_state.gd
+++ scripts/characters/states/movement_state.gd
@@ -2,59 +2,68 @@ class_name MovementState
 extends State
 
 @export var actor: Character
-@export var camera_component: ThirdPersonCamera
-
 @export_category("Animation")
-## the animation tree responsible for the animation
 @export var animation_tree: AnimationTree
-
-## the name of the blend space responsible for ground movement
 @export var blendspace: String:
 	set(value):
 		blendspace = "parameters/" + value + "/blend_position"
 		Logger.info(is_debugging, self, "blendspace set to \"" + blendspace + "\"")
 
-## the blend space value responsible for the idle animation
-@export var idle_blendspace_value: float = -1
+@export var idle_blendspace_value: float = -1.0
+@export var running_blendspace_value: float = 1.0
+@export var animation_speed: float = 10.0
+@export var movement_decay: float = 8.0
+@export var rotation_decay: float = 10.0
 
-## the blend space value responsible for the running animation
-@export var running_blendspace_value: float = 1
+# Current target blendspace value
+var target_blendspace_value: float
 
-@export var animation_speed: float = 10
-@export var animation_decay: int = 8
+func _physics_process(delta: float) -> void:
+	update_animation_target()
+	update_animation_blendspace(delta)
+	update_movement(delta)
+	update_rotation(delta)
+	actor.move_and_slide()
 
-var input_direction: Vector2 = Vector2.ZERO
-var input_vector: Vector3 = Vector3.ZERO
+func update_animation_target() -> void:
+	# Determine which animation we should be playing
+	if actor.movement_direction.is_zero_approx():
+		target_blendspace_value = idle_blendspace_value
+	else:
+		target_blendspace_value = running_blendspace_value
 
-func _physics_process(delta : float) -> void:
-	# gradually set the blendspace value to the desired value
+func update_animation_blendspace(delta: float) -> void:
 	animation_tree[blendspace] = move_toward(
 		animation_tree[blendspace], 
-		blendspace_value, 
+		target_blendspace_value, 
 		delta * animation_speed
 	)
-	
-	if actor.movement_direction.is_zero_approx():
-		change_state_signal.emit("idle")
-		return  # Exit early to avoid processing movement
+
+func update_movement(delta: float) -> void:
+	var target_speed = actor.character_stats.get_base_speed() if not actor.movement_direction.is_zero_approx() else 0.0
 	
 	actor.velocity.x = MathManager.exponential_decay(
 		actor.velocity.x, 
-		actor.movement_direction.x * actor.character_stats.get_base_speed(), 
-		animation_decay, 
+		actor.movement_direction.x * target_speed, 
+		movement_decay, 
 		delta
 	)
 	actor.velocity.z = MathManager.exponential_decay(
 		actor.velocity.z, 
-		actor.movement_direction.z * actor.character_stats.get_base_speed(), 
-		animation_decay, 
+		actor.movement_direction.z * target_speed, 
+		movement_decay, 
 		delta
 	)
-	
-	look_toward_direction(actor.movement_direction, delta)
-	actor.move_and_slide()
-	
-	
-func look_toward_direction(direction: Vector3, delta: float) -> void:
-	var target_transform := actor.pivot.global_transform.looking_at(actor.pivot.global_position + direction, Vector3.UP, true)
-	actor.pivot.global_transform = actor.pivot.global_transform.interpolate_with(target_transform, 1.0 - exp(-animation_decay * delta))
+
+func update_rotation(delta: float) -> void:
+	# Only rotate if there's a valid movement direction
+	if not actor.movement_direction.is_zero_approx():
+		var target_transform := actor.pivot.global_transform.looking_at(
+			actor.pivot.global_position + actor.movement_direction, 
+			Vector3.UP, 
+			true
+		)
+		actor.pivot.global_transform = actor.pivot.global_transform.interpolate_with(
+			target_transform, 
+			1.0 - exp(-rotation_decay * delta)
+		)
