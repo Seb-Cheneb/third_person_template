d313141b9528e518d631ed52ddd6d5d22a3cb5f8
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-07-03 12:21:46 +0300

    Last Sync: 2025-07-03 12:21 (Mobile)

---
+++ scripts/actors/players/states/player_movement_state.gd
@@ -0,0 +1,86 @@
+class_name PlayerMovementState
+extends State
+
+@export_category("Character")
+@export var actor: PlayerCharacter
+
+@export_category("Movement")
+@export var turn_speed: float = 10.0
+@export var acceleration: float = 10
+@export var deceleration: float = 8
+
+@export_category("Animation")
+@export var animation_tree: AnimationTree
+@export var blendspace: String:
+	set(value):
+		blendspace = "parameters/" + value + "/blend_position"
+		Logger.info(is_debugging, self, "blendspace set to \"" + blendspace + "\"")
+@export var idle_blendspace_value: float = -1.0
+@export var running_blendspace_value: float = 1.0
+@export var animation_speed: float = 30.0
+@export var animation_decay: float = 8.0
+
+
+func _ready() -> void:
+	super._ready()
+	actor.health_component.defeat_signal.connect(
+		func defeat_state() -> void:
+			change_state_signal.emit("Defeat")
+	)
+
+
+func _physics_process(delta: float) -> void:
+	# adjust the animation based on the direction vector of the player
+	if actor.direction.is_zero_approx():
+		update_blendspace(delta, idle_blendspace_value, animation_decay)
+		update_movement(delta, deceleration)
+	else:
+		update_blendspace(delta, running_blendspace_value, animation_speed)
+		update_movement(delta, acceleration)
+		update_rotation(delta)
+	actor.move_and_slide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		change_state_signal.emit("SlashAttack")
+	if event.is_action_pressed("right_click"):
+		change_state_signal.emit("HeavyAttack")
+		
+
+## gradually set the blendspace value to the desired value
+func update_blendspace(delta: float, blendspace_value: float, decay: float) -> void:
+	animation_tree[blendspace] = move_toward(
+		animation_tree[blendspace], 
+		blendspace_value, 
+		delta * decay
+	)
+
+
+func update_rotation(delta: float) -> void:
+	if not actor.direction.is_zero_approx():
+		var target_transform := actor.pivot.global_transform.looking_at(
+			actor.pivot.global_position + actor.direction, 
+			Vector3.UP, 
+			true
+		)
+		actor.pivot.global_transform = actor.pivot.global_transform.interpolate_with(
+			target_transform, 
+			1.0 - exp(-turn_speed * delta)
+		)
+		
+	
+func update_movement(delta: float, decay: float) -> void:
+	actor.velocity.x = MathManager.exponential_decay(
+		actor.velocity.x, 
+		actor.direction.x * actor.character_stats.get_base_speed(), 
+		decay, 
+		delta
+	)
+	
+	actor.velocity.z = MathManager.exponential_decay( 
+		actor.velocity.z, 
+		actor.direction.z * actor.character_stats.get_base_speed(), 
+		decay, 
+		delta
+	)
